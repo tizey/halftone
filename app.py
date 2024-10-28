517931899e1cd1ecb4a7878344b1d0b11585f6ba
tizey <curse28pro@gmail.com>
2024-10-28 03:21:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ import base64
 app = Flask(__name__)
 
 
-# Замените 'certs/fullchain.pem' и 'certs/private.key' на путь к вашим файлам
-app.run(ssl_context=('certs/fullchain.pem', 'certs/private.key'))
-
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 
 # Создаём папку для загрузки, если её не существует
